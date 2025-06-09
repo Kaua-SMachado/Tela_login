baa31b343d4ad2bcd7648e38f47b5b8ef87d675e
Kauã Silveira <kauasilveira.machad@gmail.com>
2025-06-08 21:21:47 -0300

    Alteração 4.0

---
+++ javascript/cadastrar.js
@@ -0,0 +1,10 @@
+function cadastrar(){
+    window.location.href = "TelaCadastrar.html"
+}
+
+
+
+function cod(){
+    //alert("Um código será enviado para seu E-Mail")
+    alert('Testando!!')
+}
---
+++ javascript/codigo.js
@@ -0,0 +1,3 @@
+function cod(){
+    alert("Um código será enviado para seu E-Mail")
+}
