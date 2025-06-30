06839f0ca281db369e848ab69562c6ae70c8ef63
Kauã Silveira <kauasilveira.machad@gmail.com>
2025-06-29 21:58:58 -0300

    Novas modificações

--- javascript/cadastrar.js
+++ javascript/cadastrar.js
@@ -2,9 +2,17 @@ function cadastrar(){
     window.location.href = "TelaCadastrar.html"
 }
 
+/*Parte do cadastro */
+function enviar(){
+    var nome = document.getElementById('nome').value;
+    var email = document.getElementById('emailCadastro').value;
+    var senha = document.getElementById('senhaCadastro').value;
+    var t = document.getElementById('teste');
 
 
-function cod(){
-    //alert("Um código será enviado para seu E-Mail")
-    alert('Testando!!')
 }
+
+/*Parte do */
+
+
+
